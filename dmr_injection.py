17aa79d642552a3499e01eb0434f025c2039e310
prbodmann <prbodmann@gmail.com>
2024-02-09 11:46:30 -0300

    created dmr injection setup

--- dmr_injection.py
+++ dmr_injection.py
@@ -102,10 +102,10 @@ while num_inj < num_injections:
                     output_data_dmr2 =  insert_fault(output_data_dmr2) 
             output_data_dmr = output_data_dmr1 # to create a array that will receive the output of the dmr selection
             for index in range( len(prob_dict[env_name])):
-                print (output_data_dmr1)
-                print (output_data_dmr2)
-                output_data_dmr = (output_data_dmr1[index] + output_data_dmr2[index])/2
-                print (output_data_dmr)
+                #print (output_data_dmr1)
+                #print (output_data_dmr2)
+                output_data_dmr[index] = (output_data_dmr1[index] + output_data_dmr2[index])/2
+                #print (output_data_dmr)
                 #if prob_dict[env_name][index] < 0.5:
                 #    output_data_dmr[index] = min(output_data_dmr1[index],output_data_dmr2[index])
                 #else:
