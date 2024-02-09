0ce85075027f75ff92f783429b22edd031f4fc69
prbodmann <prbodmann@gmail.com>
2024-02-09 09:54:31 -0300

    created dmr injection setup

--- dmr_injection.py
+++ dmr_injection.py
@@ -102,10 +102,11 @@ while num_inj < num_injections:
                     output_data_dmr2 =  insert_fault(output_data_dmr2) 
             output_data_dmr = output_data_dmr1 # to create a array that will receive the output of the dmr selection
             for index in range( len(prob_dict[env_name])):
-                if prob_dict[env_name][index] < 0.5:
-                    output_data_dmr[index] = min(output_data_dmr1[index],output_data_dmr2[index])
-                else:
-                    output_data_dmr[index] = max(output_data_dmr1[index],output_data_dmr2[index])
+                output_data_dmr = (output_data_dmr1[index] + output_data_dmr2[index])/2
+                #if prob_dict[env_name][index] < 0.5:
+                #    output_data_dmr[index] = min(output_data_dmr1[index],output_data_dmr2[index])
+                #else:
+                #    output_data_dmr[index] = max(output_data_dmr1[index],output_data_dmr2[index])
             obs_dmr, reward_dmr, done_dmr, inf_dmr = env_dmr.step(tf.convert_to_tensor(output_data_dmr))
             step_counter_dmr += 1  
         if not done_np:
