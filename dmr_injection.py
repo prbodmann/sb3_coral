7a757a69c8338141e866f7363e52bb2d9e20cb43
prbodmann <prbodmann@gmail.com>
2024-02-26 11:38:58 -0300

    created dmr injection setup

--- dmr_injection.py
+++ dmr_injection.py
@@ -1,10 +1,10 @@
 import mujoco_py
 import gym
 import sys
-import tflite_runtime.interpreter as tflite
+
 import tensorflow as tf
 import random
-from pycoral.utils.edgetpu import make_interpreter
+
 
 
 
@@ -16,10 +16,10 @@ prob_dict = {
 }
 
 limit_dict = {
-    "Walker2d-v3":[-10,10],
-    "Hopper-v3":[-90,90],
-    "Humanoid-v3":[-50,50],
-    "HalfCheetah-v3":[-10,10]
+    "Walker2d-v3":10,
+    "Hopper-v3":90,
+    "Humanoid-v3":50,
+    "HalfCheetah-v3":10
 }
 
 
@@ -34,16 +34,29 @@ else:
     num_injections=int(sys.argv[3])
 
 rng1 = random.Random()
+previous_selected_core = 0
 
+def create_interpreter(env_name, cpu=False, device=":0"):
+    if cpu:
+        model_save_file = "./"+env_name+"_quant.tflite"
+        from tensorflow import lite as tflite
+        interpreter = tflite.Interpreter(model_save_file)
+    else:
+        model_save_file = "./"+env_name+"_quant_edgetpu.tflite"
+        from pycoral.utils.edgetpu import make_interpreter
+        interpreter = make_interpreter(model_save_file, device=device)
 
-def insert_fault(output_rl,error_mag,error_dist):
+    return interpreter
+
+def insert_fault(output_rl):
     global rng1, first_errouneous_step
-   
+    #return output_rl
     liest_random_index = rng1.sample(range(len(output_rl)),rng1.randint(1,len(output_rl) ) )
     #print(liest_random_index)
     wrong_array = output_rl
     for i in liest_random_index:
-        
+        error_mag = rng1.uniform(0,limit_dict[env_name])
+        error_dist = rng1.random()
         #print(error_dist)
         if error_dist < prob_dict[env_name][i]:
             #print("sum")
@@ -53,14 +66,104 @@ def insert_fault(output_rl,error_mag,error_dist):
             wrong_array[i] -= error_mag
     return wrong_array
 
-model_save_file = "./"+env_name+"_quant_edgetpu.tflite"
+def select_copy(output_data_dmr1,output_data_dmr2):
+    global previous_selected_core
+    count_0=0
+    count_1=0
+    for index in range( len(prob_dict[env_name])):
+        #print (output_data_dmr1)
+        #print (output_data_dmr2)
+        #output_data_dmr[index] = (output_data_dmr1[index] + output_data_dmr2[index])/2
+        #print (output_data_dmr)
+        if prob_dict[env_name][index] > 0.5:
+            #output_data_dmr[index] = min(output_data_dmr1[index],output_data_dmr2[index])
+            if output_data_dmr1[index] < output_data_dmr2[index]:
+                count_0 += 1
+            elif output_data_dmr1[index] > output_data_dmr2[index]:
+                count_1 += 1
+            else:
+                count_0 +=1 
+                count_1 +=1
+            
+        else:
+            #output_data_dmr[index] = max(output_data_dmr1[index],output_data_dmr2[index])
+            if output_data_dmr1[index] > output_data_dmr2[index]:
+                count_0 += 1
+            elif output_data_dmr1[index] < output_data_dmr2[index]:
+                count_1 += 1
+            else:
+                count_0 +=1 
+                count_1 +=1
+    if  count_0 > count_1:
+        #print(f"worng core: {select_core}, core selected: core 0")
+        output_data_dmr = output_data_dmr1
+        previous_selected_core = 0
+    elif count_0 < count_1:
+        #print(f"worng core: {select_core}, core selected: core 1")
+        output_data_dmr = output_data_dmr2
+        previous_selected_core = 1
+    else:
+        #if j>first_errouneous_step:
+        #    print(f"dmr 0 {output_data_dmr1 - output_data_dmr2}")
+        #input()
+        if previous_selected_core == 0:
+            #print(f"worng core: {select_core}, core selected: core 0 equal")
+            output_data_dmr = output_data_dmr1
+        else:
+            #print(f"worng core: {select_core}, core selected: core 1 equal")
+            output_data_dmr = output_data_dmr2
+    count_0=0
+    count_1=0
+    for index in range( len(prob_dict[env_name])):
+        #print (output_data_dmr1)
+        #print (output_data_dmr2)
+        #output_data_dmr[index] = (output_data_dmr1[index] + output_data_dmr2[index])/2
+        #print (output_data_dmr)
+        if prob_dict[env_name][index] > 0.5:
+            #output_data_dmr[index] = min(output_data_dmr1[index],output_data_dmr2[index])
+            if output_data_dmr1[index] < output_data_dmr2[index]:
+                count_0 += 1
+            elif output_data_dmr1[index] > output_data_dmr2[index]:
+                count_1 += 1
+            else:
+                count_0 +=1 
+                count_1 +=1
+            
+        else:
+            #output_data_dmr[index] = max(output_data_dmr1[index],output_data_dmr2[index])
+            if output_data_dmr1[index] > output_data_dmr2[index]:
+                count_0 += 1
+            elif output_data_dmr1[index] < output_data_dmr2[index]:
+                count_1 += 1
+            else:
+                count_0 +=1 
+                count_1 +=1
+    if  count_0 > count_1:
+        #print(f"worng core: {select_core}, core selected: core 0")
+        output_data_dmr = output_data_dmr1
+        previous_selected_core = 0
+    elif count_0 < count_1:
+        #print(f"worng core: {select_core}, core selected: core 1")
+        output_data_dmr = output_data_dmr2
+        previous_selected_core = 1
+    else:
+        #if j>first_errouneous_step:
+        #    print(f"dmr 0 {output_data_dmr1 - output_data_dmr2}")
+        #input()
+        if previous_selected_core == 0:
+            #print(f"worng core: {select_core}, core selected: core 0 equal")
+            output_data_dmr = output_data_dmr1
+        else:
+            #print(f"worng core: {select_core}, core selected: core 1 equal")
+            output_data_dmr = output_data_dmr2
+    return output_data_dmr
 env_dmr = gym.make(env_name)
 env_not_protected = gym.make(env_name)
 
 
-interpreter_dmr1 = make_interpreter(model_save_file)
-interpreter_dmr2 = make_interpreter(model_save_file)
-interpreter_not_protected = make_interpreter(model_save_file)
+interpreter_dmr1 = create_interpreter(env_name,cpu=True)
+interpreter_dmr2 = create_interpreter(env_name,cpu=True)
+interpreter_not_protected = create_interpreter(env_name,cpu=True)
 
 interpreter_dmr1.allocate_tensors()
 interpreter_dmr2.allocate_tensors()
@@ -75,27 +178,27 @@ output_details = interpreter_dmr1.get_output_details()
 num_inj = 0
 
 while num_inj < num_injections:
-    first_errouneous_step = rng1.randint(0, 1000)
+    first_errouneous_step = rng1.randint(0, 511)
     step_counter_dmr = 0
     step_counter_np = 0
     done_dmr=False
     done_np=False
     random.seed(0)
 
-    env_not_protected.seed(seed)
-    obs_np = env_not_protected.reset()
+    #env_not_protected.seed(seed)
+    obs_np, _ = env_not_protected.reset(seed=seed)
 
-    env_dmr.seed(seed)
-    obs_dmr = env_dmr.reset()
+    #env_dmr.seed(seed)
+    obs_dmr, _= env_dmr.reset(seed=seed)
 
     select_core=rng1.randint(0, 1)
     output_data_dmr = [0]*len(prob_dict[env_name])
     for j in range(1000):
         previous_selected_core = rng1.randint(0, 1)
-        if j>first_errouneous_step:
-            error_mag = rng1.uniform(limit_dict[env_name][0],limit_dict[env_name][1])
-            error_dist = rng1.random()
+
+            
         if not done_dmr:
+            #print(obs_dmr)
             input_dmr = tf.cast(obs_dmr.reshape(1, -1),tf.float32)
             interpreter_dmr1.set_tensor(input_details[0]['index'], input_dmr)
             interpreter_dmr2.set_tensor(input_details[0]['index'], input_dmr)
@@ -105,59 +208,16 @@ while num_inj < num_injections:
             output_data_dmr2 = interpreter_dmr2.get_tensor(output_details[0]['index'])[0]
             if j>first_errouneous_step:
                 if  select_core== 0:
-                    output_data_dmr1 =  insert_fault(output_data_dmr1,error_mag,error_dist) 
+                    output_data_dmr1 =  insert_fault(output_data_dmr1) 
                     output_data_dmr2 = output_data_dmr2
                 else:
                     output_data_dmr1 = output_data_dmr1
-                    output_data_dmr2 =  insert_fault(output_data_dmr2,error_mag,error_dist)
+                    output_data_dmr2 =  insert_fault(output_data_dmr2)
             #print(output_data_dmr1 - output_data_dmr2)
 
              # to create a array that will receive the output of the dmr selection
-            count_0=0
-            count_1=0
-            for index in range( len(prob_dict[env_name])):
-                #print (output_data_dmr1)
-                #print (output_data_dmr2)
-                #output_data_dmr[index] = (output_data_dmr1[index] + output_data_dmr2[index])/2
-                #print (output_data_dmr)
-                if prob_dict[env_name][index] > 0.5:
-                    #output_data_dmr[index] = min(output_data_dmr1[index],output_data_dmr2[index])
-                    if output_data_dmr1[index] < output_data_dmr2[index]:
-                        count_0 += 1
-                    elif output_data_dmr1[index] > output_data_dmr2[index]:
-                        count_1 += 1
-                    else:
-                        count_0 +=1 
-                        count_1 +=1
-                    
-                else:
-                    #output_data_dmr[index] = max(output_data_dmr1[index],output_data_dmr2[index])
-                    if output_data_dmr1[index] > output_data_dmr2[index]:
-                        count_0 += 1
-                    elif output_data_dmr1[index] < output_data_dmr2[index]:
-                        count_1 += 1
-                    else:
-                        count_0 +=1 
-                        count_1 +=1
-            if  count_0 > count_1:
-                #print(f"worng core: {select_core}, core selected: core 0")
-                output_data_dmr = output_data_dmr1
-                previous_selected_core = 0
-            elif count_0 < count_1:
-                #print(f"worng core: {select_core}, core selected: core 1")
-                output_data_dmr = output_data_dmr2
-                previous_selected_core = 1
-            else:
-                #if j>first_errouneous_step:
-                #    print(f"dmr 0 {output_data_dmr1 - output_data_dmr2}")
-                #input()
-                if previous_selected_core == 0:
-                    #print(f"worng core: {select_core}, core selected: core 0 equal")
-                    output_data_dmr = output_data_dmr1
-                else:
-                    #print(f"worng core: {select_core}, core selected: core 1 equal")
-                    output_data_dmr = output_data_dmr2
-            obs_dmr, reward_dmr, done_dmr, inf_dmr = env_dmr.step(tf.convert_to_tensor(output_data_dmr))
+            output_data_dmr=select_copy(output_data_dmr1,output_data_dmr2)
+            obs_dmr, reward_dmr, done_dmr, someting, inf_dmr = env_dmr.step(tf.convert_to_tensor(output_data_dmr))
             step_counter_dmr += 1  
         if not done_np:
             input_np = tf.cast(obs_np.reshape(1, -1),tf.float32)
@@ -165,13 +225,13 @@ while num_inj < num_injections:
             interpreter_not_protected.invoke()
             output_data_not_protected = interpreter_not_protected.get_tensor(output_details[0]['index'])[0]
             if j>first_errouneous_step:
-                output_data_not_protected=insert_fault(output_data_not_protected,error_mag,error_dist)  
-            obs_np, reward_np, done_np, info_np = env_not_protected.step(output_data_not_protected)
+                output_data_not_protected=insert_fault(output_data_not_protected)  
+            obs_np, reward_np, done_np, someting,info_np = env_not_protected.step(output_data_not_protected)
             step_counter_np += 1
 
 
         if done_np and done_dmr:            
-            print(f"First errouneous step: {first_errouneous_step} ----- dmr: num_steps: {step_counter_dmr} ----- not_proteted: num_steps: {step_counter_np}")
+            print(f"{first_errouneous_step} {step_counter_dmr} {step_counter_np}")
             break
     num_inj+=1
    
