2f44db16cd427c06c67562f04dc48008cb4f67d0
prbodmann <prbodmann@gmail.com>
2024-02-29 16:43:54 -0300

    modified

--- dmr_injection.py
+++ dmr_injection.py
@@ -69,7 +69,7 @@ def insert_fault(output_rl):
 def select_copy(output_data_dmr1,output_data_dmr2,counter):
 
     global previous_selected_core
-    if np.all(output_data_dmr1 output_data_dmr2):
+    if np.all(output_data_dmr1 == output_data_dmr2):
         return output_data_dmr1
     if counter > 0:
         if previous_selected_core == 0:
